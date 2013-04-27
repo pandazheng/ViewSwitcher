27efef63faf6c2241486a4a3b5b2124f22a8ae53
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-04-27 06:12:28 -0700

    Initial Commit

---
+++ ViewSwitcher/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  ViewSwitcher
+//
+//  Created by panda zheng on 13-4-27.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewSwitcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewSwitcher
+//
+//  Created by panda zheng on 13-4-27.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
